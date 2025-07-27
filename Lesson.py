8b5a4627e8171c762ff377b75f4accb4b977215f
Enes Salih Gökçek <118908311+enesgokcek1@users.noreply.github.com>
2025-07-27 19:32:47 +0300

    Update Lesson.py

--- Lesson.py
+++ Lesson.py
@@ -1,1 +1,8 @@
-class lesson:    def __init__(self, id,Name):        if id is None:            self.id = 0        else:            self.id = 0        self.Name = Name    
+class lesson:
+    def __init__(self, id,Name):
+        if id is None:
+            self.id = 0
+        else:
+            self.id = 0
+        self.Name = Name
+    
