21f8ecd96203b7d89e10ebbc9227f46033b8f4c6
Enes Salih Gökçek <118908311+enesgokcek1@users.noreply.github.com>
2025-07-27 19:33:09 +0300

    Update Teacher.py

--- Teacher.py
+++ Teacher.py
@@ -1,1 +1,12 @@
-class Teacher:    def __init__(self, id,Branch,Name,Surname,Birthday,Gender):        if id is None:            self.id = 0        else:            self.id = 0        self.Branch = Branch        self.Name = Name        self.Surname = Surname        self.Birthday = Birthday        self.Gender = Gender      
+class Teacher:
+    def __init__(self, id,Branch,Name,Surname,Birthday,Gender):
+        if id is None:
+            self.id = 0
+        else:
+            self.id = 0
+        self.Branch = Branch
+        self.Name = Name
+        self.Surname = Surname
+        self.Birthday = Birthday
+        self.Gender = Gender
+      
